9dfbd98db738a6588b8ec4983f068319b8d772cc
sunil <sunil@codebee.dk>
2021-04-13 12:19:17 +0545

    POS-2 added webclientprint

---
+++ ThermalPrinter/Controllers/InvoiceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ThermalPrinter.Models;
+using ThermalPrinter.Models.DataManager;
+
+namespace ThermalPrinter.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private readonly InvoiceDataManager _invoiceDataManager;
+
+        public InvoiceController()
+        {
+            _invoiceDataManager = new InvoiceDataManager();
+        }
+        // GET: Invoice
+        public List<InvoiceModel> GetInvoiceByOrderId(int orderId)
+        {
+            var list = new List<InvoiceModel>();
+            try
+            {
+                list = _invoiceDataManager.GetInvoiceByOrderId(orderId);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return list;
+        }
+    }
+}
---
+++ ThermalPrinter/Models/ViewModel/InvoiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ThermalPrinter.Models
+{
+    public class InvoiceModel
+    {
+        public string Items { get; set; }
+        public int Quantity { get; set; }
+        public int Rate { get; set; }
+        public decimal Total { get; set; }
+    }
+}
